aa823fb352546df05921b28bef8772817c40c189
holczben <133602035+holczben@users.noreply.github.com>
2025-06-05 09:02:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-document.getElementById('hamburger').onclick = function() {
+document.getElementById('hamburger').onclick = hamburger;
+
+function hamburger() {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
+    scroll();
     const links = document.getElementById('links');
     links.style.display  = links.style.display == 'flex' ? 'none' : 'flex';
     console.log(links.style.display)
 }
+
+function scroll() {
+    const current = document.body.style.overflowY;
+    const newVal = current === 'unset' || current === '' ? 'hidden' : 'unset';
+    document.body.style.overflowY = newVal;
+    document.documentElement.style.overflowY = newVal;
+}
+
+document.querySelectorAll('.link').forEach(link => {
+  link.addEventListener('click', e => {
+    hamburger()
+  });
+});
